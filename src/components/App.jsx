178629189ba72818693930d2c088a0d604edc6bb
crigigal <50228889+crigigal@users.noreply.github.com>
2021-07-18 19:14:45 +0100

    Final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function App() {
-  return <div />;
+  const [count, setCount] = React.useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
